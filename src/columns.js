c58548230ce7175b5cafe85d082cbc9e89363f55
bokuweb <bokuweb12@gmail.com>
2016-02-07 19:12:18 +0900

    [WIP] Seperate style

--- src/columns.js
+++ src/columns.js
@@ -5,39 +5,38 @@ export default class Columns extends Component {
   render () {
     return (
       <InlineCss stylesheet={`
-        & .columns:not(:last-child) {
+        &:not(:last-child) {
           margin-bottom: 20px;
         }
 
         @media screen and (min-width: 769px) {
-          & .columns {
+          & {
             display: flex;
             width: 100%;
           }
         }
 
-        & .columns.is-gapless > .column + .column {
+        &.is-gapless > .column + .column {
           margin: 0;
         }
 
-        & .columns.is-vcentered {
+        &.is-vcentered {
           align-items: center;
         }
 
         @media screen and (min-width: 769px) {
-          & .columns.is-grid {
+          &.is-grid {
             flex-wrap: wrap;
           }
-          & .columns.is-grid > .column {
+          &.is-grid > .column {
             flex-basis: 33.3333%;
             max-width: 33.3333%;
             padding: 10px;
             width: 33.3333%;
           }
-          & .columns.is-grid > .column + .column {
+          &.is-grid > .column + .column {
             margin-left: 0;
-        }`}>
-        <div className="columns">{this.props.children}</div>
+        }`}>{this.props.children}
       </InlineCss>
     );
   }
